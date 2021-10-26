ce588ad4c4c7ba6c9ae83b2b129e08364193a211
Marco Milesi <milesimarco@users.noreply.github.com>
2021-10-26 22:20:17 +0200

    Update wp-spid-italia.php

--- wp-spid-italia.php
+++ wp-spid-italia.php
@@ -3,7 +3,7 @@
 Plugin Name: WP SPID Italia
 Description: SPID - Sistema Pubblico di Identità Digitale
 Author: Marco Milesi
-Version: 2.2.2
+Version: 2.2.3
 Author URI: http://www.marcomilesi.com
 */
 
